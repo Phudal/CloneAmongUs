49f0a728d28b3ec63b8f58a544677c80040bcd3a
Phudal <080_075@naver.com>
2021-08-14 18:01:29 +0900

    08-14 commit
    c++에 있는 __FILE__, __LINE__을 C#으로 구현해보았다... 디버깅할 떄 요긴한게 쓰일것 같다.

---
+++ Assets/01_Scripts/Debugging/DebugClass.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using UnityEngine;
+using System.Runtime.CompilerServices;
+
+public static class DebugClass
+{
+    // __LINE__
+    private static System.Diagnostics.StackFrame stackFrameForLine = new System.Diagnostics.StackFrame(true);
+
+    // __FILE__
+    private static System.Diagnostics.StackFrame stackFrameForFile = new System.Diagnostics.StackFrame(true);
+
+    public static string __LINE__([CallerLineNumber] int line = 0)
+    {
+        return string.Intern($"{line}");
+        // return stackFrameForLine.GetFileLineNumber().ToString();
+    }
+
+    public static string __FILE__([CallerFilePath] string file = "")
+    {
+        return string.Intern($"{Path.GetFileName(file)}");
+        // return stackFrameForFile.GetFileName();
+    }
+}
---
+++ Assets/01_Scripts/Debugging/Temp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Temp : MonoBehaviour
+{
+    // this is temp func
+    private void Update()
+    {
+        Debug.Log(DebugClass.__FILE__());
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        Debug.Log(DebugClass.__LINE__());
+    } 
+}
