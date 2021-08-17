8cf44d47a1471b06b27dacd1dd7039a09ee2bf1e
Phudal <080_075@naver.com>
2021-08-17 21:18:05 +0900

    08-17 commit
    Unreal에 있던 로그 함수들 구현 완료

--- Assets/01_Scripts/Debugging/Temp.cs
+++ Assets/01_Scripts/Debugging/Temp.cs
@@ -1,23 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Temp : MonoBehaviour
 {
+    private void Start()
+    {
+        UN_LOG.LogWithFileName("this is start - file");
+        UN_LOG.LogWithLineNumber("this is start - line");
+        UN_LOG.LogWithFileAndLine("this is start - file and line");
+    }
+
     // this is temp func
+    /*
     private void Update()
     {
-        Debug.Log(DebugClass.__FILE__());
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        Debug.Log(DebugClass.__LINE__());
-    } 
+        Debug.Log(UN_LOG.__FILE__());
+
+        Debug.Log(UN_LOG.__LINE__());
+    } */ 
 }
---
+++ Assets/01_Scripts/Debugging/UnityLogClass.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using UnityEngine;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+using Debug = UnityEngine.Debug;
+
+public static class UN_LOG
+{
+    // __LINE__
+    private static System.Diagnostics.StackFrame _stackFrameForLine = new System.Diagnostics.StackFrame(true);
+
+    // __FILE__
+    private static System.Diagnostics.StackFrame _stackFrameForFile = new System.Diagnostics.StackFrame(true);
+
+    // 로그 파일 생성 경로
+    static public string LogPath = "";
+    
+    
+    public static string __LINE__([CallerLineNumber] int line = 0)
+    {
+        return string.Intern($"{line}");
+        // return stackFrameForLine.GetFileLineNumber().ToString();
+    }
+
+    public static string __FILE__([CallerFilePath] string file = "")
+    {
+        return string.Intern($"{Path.GetFileName(file)}");
+        // return stackFrameForFile.GetFileName();
+    }
+
+    // 로그 경로 설정
+    public static void SetLogPath()
+    {
+        // 폴더 이름
+        string directoryName = "/Log";
+        
+        // 로그 파일 이름
+        string fileName = "Log.txt";
+        
+        // 폴더 경로를 저장하기 위한 변수
+        string path = "";
+
+        path = (Application.dataPath + directoryName);
+        LogPath = (Application.dataPath + directoryName + "/" + fileName);
+
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+    }
+
+    public static void Log(string msg)
+    {
+        // 시스템 로그 기록
+        Debug.Log(msg);
+        
+        // 파일 경로가 만들어지지 않은 상태라면 파일경로 생성 함수로 생성
+        if (LogPath == "")
+        {
+            SetLogPath();
+        }
+
+        FileStream tFile = null;
+        
+        // 파일 확인 후 없다면 생성
+        if (!File.Exists(LogPath))
+        {
+            tFile = new FileStream(LogPath, FileMode.Create, FileAccess.Write);
+        }
+        
+        // 파일이 있으면 내용 추가
+        else
+        {
+            tFile = new FileStream(LogPath, FileMode.Append);
+        }
+        
+        // 열린 파일이 크기가 크디면 닫고 새 파일 스트림으로 생성
+        if (tFile.Length > 1048000)
+        {
+            tFile.Close();
+            tFile = new FileStream(LogPath, FileMode.Create, FileAccess.Write);
+        }
+
+        StreamWriter tSW = new StreamWriter(tFile);
+        
+        // 로그 내용 앞에 시간 추가
+        string tLogfrm = DateTime.Now.ToString("mm-dd hh::mm::ss") + " -- " + msg;
+        
+        // 로그 기록
+        tSW.WriteLine(tLogfrm);
+        
+        // 사용했던 스트림들 닫기
+        tSW.Close();
+        tFile.Close();
+    }
+
+    public static void LogWithFileName(string msg, [CallerFilePath] string file = "")
+    {
+        // Log("File: " + __FILE__() + " | " + _msg);
+        Log("File: " + string.Intern($"{Path.GetFileName(file)}") + " | " + msg);
+    }
+
+    public static void LogWithLineNumber(string msg, [CallerLineNumber] int line = 0)
+    {
+        Log("Line: " + string.Intern($"{line}") + " | " + msg);
+    }
+
+    public static void LogWithFileAndLine(string msg, 
+        [CallerFilePath] string file = "",
+        [CallerLineNumber] int line = 0)
+    {
+        // Log("File: " + __FILE__() + " | " + "Line: " + __LINE__() + " | " + _msg);
+        Log("File: " + string.Intern($"{Path.GetFileName(file)}") + " | " +
+            "Line: " + string.Intern($"{line}") + " | " + msg);
+    }
+}
