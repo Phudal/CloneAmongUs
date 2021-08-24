9cefeb332f9943e5b493da90f189b1a38a0381e8
Phudal <080_075@naver.com>
2021-08-24 21:21:37 +0900

    08-24 commit
    맵 구현

---
+++ Assets/01_Scripts/Map Animation/WeaponPipeLight.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponPipeLight : MonoBehaviour
+{
+    private Animator _animator;
+
+    private WaitForSeconds wait = new WaitForSeconds(0.15f);
+
+    private List<WeaponPipeLight> lights = new List<WeaponPipeLight>();
+    private static readonly int On = Animator.StringToHash("On");
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            var child = transform.GetChild(i).GetComponent<WeaponPipeLight>();
+            if (child)
+            {
+                lights.Add(child);
+            }
+        }
+    }
+
+    public void TurnOnLight()
+    {
+        _animator.SetTrigger(On);
+        StartCoroutine(TurnOnLightAtChild());
+    }
+
+    private IEnumerator TurnOnLightAtChild()
+    {
+        yield return wait;
+        
+        foreach (var child in lights)
+        {
+            child.TurnOnLight();
+        }
+    }
+}
